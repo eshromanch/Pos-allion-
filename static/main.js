7acec5968302fe90e34efa40b03574354140c403
unknown <romanch.haq868@gmail.com>
2021-10-08 03:12:10 +0600

    IDK THE NUMBER

--- static/main.js
+++ static/main.js
@@ -1,55 +1,89 @@
-$(document).on("click","#cAdd", function(e){
-    e.preventDefault();
-    $.ajax(
-        {
-        type: "POST",
-        url : "" ,
-        data: {
-            customerName: $("#customerName").value,
-            customerId: $("#customerId").value,
-            shopName:  $("#shopName").value,
-            address: $("#address").value,
-            phoneNumber: $("#phoneNumber").value,
-            srNumber: $("#srNumber").value,
-            // csrf: ,
-            // action: ,
-        },
-        processData: false,
-        contentType: false,
-    success: {
-        function(){
+const addMore = document.getElementById("pAdd");
+const tForm = document.getElementById("t");
+addMore.addEventListener("click", function(e){
+    e.preventDefault()
+
+    let newDiv =  document.createElement('div');
+    newDiv.setAttribute('class', 'groupT');
+    tForm.appendChild(newDiv)
+    newDiv.innerHTML= `<input id="pName" name="pName" type="text" placeholder="Products name">
+    <input id="productId" name="productId" type="text" placeholder="Product id">
+    <input id="pPrice" name="pPrice" type="text" placeholder="Selling price">
+    <input id="qty" type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="qty " size="4" pattern="" inputmode="" placeholder="qty">
+    <input id="disc" name="disc" type="text" placeholder="Discount:">
+    <input id="amount" name="amount" type="text" placeholder="Amount:">
+
+    <input id="stock" name="stock" type="text" placeholder="stock:">
+    <input id="total" name="total" type="text" placeholder="Total:">
+    <button type="button" id="tAdd" class="add"><i class="fas fa-trash"></i></button>`
+  
+    newDiv.addEventListener("click", function(e){
+        const item = e.target;
+        if(item.classList[0]==="add"){
+            const dlt = item.parentElement;
+            dlt.classList="remove";
+            dlt.addEventListener("transitioned",function(){
+              dlt.remove();
+            });
+          }
+        // newDiv.classList='remove'
+    })
+    
+})
+
+
+// $(document).on("click","#cAdd", function(e){
+//     e.preventDefault();
+//     $.ajax(
+//         {
+//         type: "POST",
+//         url : "" ,
+//         data: {
+//             customerName: $("#customerName").value,
+//             customerId: $("#customerId").value,
+//             shopName:  $("#shopName").value,
+//             address: $("#address").value,
+//             phoneNumber: $("#phoneNumber").value,
+//             srNumber: $("#srNumber").value,
+//             // csrf: ,
+//             // action: ,
+//         },
+//         processData: false,
+//         contentType: false,
+//     success: {
+//         function(){
             
-        }
-    }
+//         }
+//     }
 
-})
-})
+// })
+// })
 
-$(document).on("click","#pAdd", function(e){
+// $(document).on("click","#pAdd", function(e){
 
-    e.preventDefault();
-    $.ajax(
-        {
-        type: "POST",
-        url : "" ,
-        data: {
-            pName: $("#pName").value,
-            productId: $("#productId").value,
-            pPrice:  $("#pPrice").value,
-            qty: $("#qty").value,
-            // csrf: ,
-            // action: ,
-        },
-        processData: false,
-        contentType: false,
-    success: {
-        function(){
-            $("form").reset()
-        }
-    }
+//     e.preventDefault();
+//     $.ajax(
+//         {
+//         type: "POST",
+//         url : "" ,
+//         data: {
+//             pName: $("#pName").value,
+//             productId: $("#productId").value,
+//             pPrice:  $("#pPrice").value,
+//             qty: $("#qty").value,
+//             // csrf: ,
+//             // action: ,
+//         },
+//         processData: false,
+//         contentType: false,
+//     success: {
+//         function(){
+//             $("form").reset()
+//         }
+//     }
 
-})
-})
+// })
+// })
 
 
 
