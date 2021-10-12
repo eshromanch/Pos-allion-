4e40fe0044e04565186918a40912f6c38d1a4e18
unknown <romanch.haq868@gmail.com>
2021-10-12 11:51:17 +0600

    invoice er mayere bap

--- static/main.js
+++ static/main.js
@@ -1,111 +1,72 @@
 const addMore = document.getElementById("pAdd");
+const caddMore = document.getElementById("cAdd");
 const tForm = document.getElementById("t");
 let table = document.getElementById("table")
-let counter = 1;
+let tableOne = document.getElementById("table1")
 
-// function addNumberT() {
-//   var x = document.getElementById("pPrice").value;
-//   var y =   document.getElementById("qty '+counter'").value;
-//   var z = document.getElementById("amount").value;
-//   var disc = document.getElementById("disc").value
-//   var discTotal = y * x * disc / 100 ;
-//   document.getElementById("total + '+counter+' ").value = (y * x ) ;
-// }
-// addMore.addEventListener("click", function(e){
-//   counter+=1
-//     e.preventDefault()
-//     let newDiv =  document.createElement('div');
-//     newDiv.setAttribute('class', 'groupT');
-//     tForm.appendChild(newDiv)
-
-//     // let pPrce =  document.createElement('input');
-//     // pPrce.setAttribute('id', 'pPrice' + counter++);
-//     // pPrce.setAttribute('oninput',"addNumberT()");
-//     // newDiv.appendChild(pPrce)
-
-//     // let qty =  document.createElement('input');
-//     // qty.setAttribute('id', 'qty' + counter++);
-//     // qty.setAttribute('type', 'number');
-//     // qty.setAttribute('oninput', "addNumberT()" );
-//     // newDiv.appendChild(qty)
-
-//     // let total =  document.createElement('input');
-//     // total.setAttribute('id', 'total' + counter++);
-    
-//     // newDiv.appendChild(total)
-//     // newDiv.innerHTML= '<input id="pName" name="pName" type="text" placeholder="Products name">\
-//     // <input id="productId" name="productId" type="text" placeholder="Product id">\
-//     // <input id="pPrice'+counter+' "  oninput="addNumber()" name="pPrice" type="text" placeholder="Selling price">\
-//     // <input id="qty'+counter+' "  oninput="addNumber()" type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="qty " size="4" pattern="" inputmode="" placeholder="qty">\
-//     // <input id="total'+counter+'" name="total" type="text" placeholder="Total: ">\
-//     // '
-
-//     newDiv.innerHTML= `<input id="pName" name="pName" type="text" placeholder="Products name">
-//     <input id="productId" name="productId" type="text" placeholder="Product id">
-//     <input id="pPrice'+counter+' "  oninput="addNumber()" name="pPrice" type="text" placeholder="Selling price">
-//     <input id="qty'+counter+' "  oninput="addNumber()" type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="qty " size="4" pattern="" inputmode="" placeholder="qty">
-//     <input id="disc +counter+' " oninput="addNumber()" name="disc" type="text" placeholder="Discount:">
-//     <input id="amount'+counter+' " oninput="addNumber()" name="amount" type="text" placeholder="Amount:">
-//     <input id="stock" name="stock" type="text" placeholder="stock:">
-//     <input id="total'+counter+'" name="total" type="text" placeholder="Total: ">
-//     <button type="button" id="tAdd" class="add"><i class="fas fa-trash"></i></button>`
-
-    
-    
-
-//     newDiv.addEventListener("click", function(e){
-//         const item = e.target;
-//         if(item.classList[0]==="add"){
-//             const dlt = item.parentElement;
-//             dlt.classList="remove";
-//             dlt.addEventListener("transitioned",function(){
-//               dlt.remove();
-//             });
-//           }
-         
-//         // newDiv.classList='remove'
-//     })
-
-// })
-
-
+//this array display data 
 let orders = []
-
-let headers = ["Product Name" , "Price" ,"Qty" ,"Total"] 
+//this array send all product data 
+let ordersDetails = []
+//this array send all customer data 
+let customer = []
 
 addMore.addEventListener("click", function (e){
   e.preventDefault()
+  //this object for display data 
   let order ={
     productName: document.getElementById("pName").value,
     productPrice: document.getElementById("pPrice").value,
     qty: document.getElementById("qty").value,
-    total: document.getElementById("total").value
+    total: document.getElementById("total").value,
+   
   }
   orders.push(order)
-  tForm.reset();
-  // let table = document.createElement("table")
 
-  // let headerRow = document.createElement("tr")
-  // headers.forEach(headerText => {
-  //   let header = document.createElement("th")
-  //   let texNode = document.createTextNode(headerText)
-  //   header.appendChild(texNode)
-  //   headerRow.appendChild(header)
-  // })
-  // table.appendChild(headerRow)
-  orders.forEach(allOrder => {
-    let row = document.createElement("tr")
-    Object.values(allOrder).forEach(text => {
-      let cell = document.createElement("td")
-      let texNode = document.createTextNode(text)
-      cell.appendChild(texNode)
-      row.appendChild(cell)
-    })
-    table.appendChild(row)
+  //this is product object 
+
+  let productOrder ={
+    productName: document.getElementById("pName").value,
+    productId: document.getElementById("productId").value,
+    productPrice: document.getElementById("pPrice").value,
+    qty: document.getElementById("qty").value,
+    disc: document.getElementById("disc").value,
+    amount: document.getElementById("amount").value,
+    disc: document.getElementById("stock").value,
+    total: document.getElementById("total").value,
+   
+  }
+  ordersDetails.push(productOrder)
+  tForm.reset();
+  
+  $.ajax(
+    {
+    type: "POST",
+    url : "" ,
+    data: ordersDetails, //all product data 
+    processData: false,
+    contentType: false,
+    csrf: $("input[csrfmiddlewaretoken]").value,
+success: {
+    function(newOrder){
+        console.log("success")
+    }
+}
+
+})
+let row; 
+  
+orders.forEach(allOrder => {
+  row = document.createElement("tr")
+  Object.values(allOrder).forEach(text => {
+    let cell = document.createElement("td")
+    let texNode = document.createTextNode(text)
+    cell.appendChild(texNode)
+    row.appendChild(cell)
   })
- 
-  let rightSdie = document.querySelector(".right")
-  rightSdie.appendChild(table)
+})
+
+table.appendChild(row) 
 })
 
 
@@ -117,27 +78,64 @@ function addNumber() {
   var discTotal = y * x * disc / 100 ;
  document.getElementById("total").value = (y * x ) - z - discTotal;
 }
-
-
-
-
-$(document).on("click","#cAdd", function(e){
-    e.preventDefault();
-    $.ajax(
-        {
-        type: "POST",
-        url : "" ,
-        data: orders,
-        processData: false,
-        contentType: false,
-    success: {
-        function(){
-            console.log("success")
-        }
-    }
-
+caddMore.addEventListener("click", function(e){
+  e.preventDefault()
+  let information ={
+    customerName: document.getElementById("customerName").value,
+    customerId: document.getElementById("customerId").value,
+    shopName: document.getElementById("shopName").value,
+    address: document.getElementById("address").value,
+    qty: document.getElementById("phoneNumber").value,
+    total: document.getElementById("srNumber").value,
+  }
+  customer.push(information)
+  let row; 
+  
+customer.forEach(allOrder => {
+  row = document.createElement("tr")
+  Object.values(allOrder).forEach(text => {
+    let cell = document.createElement("td")
+    let texNode = document.createTextNode(text)
+    cell.appendChild(texNode)
+    row.appendChild(cell)
+  })
 })
+
+tableOne.appendChild(row) 
 })
+
+
+
+// $(document).on("click","#cAdd", function(e){
+//     e.preventDefault();
+//     $.ajax(
+//         {
+//         type: "POST",
+//         url : "" ,
+//         data: orders,
+//         processData: false,
+//         contentType: false,
+//     success: {
+//         function(){
+//             console.log("success")
+//         }
+//     }
+
+// })
+// })
+
+//don't delete comments for now .. I might need it later.
+
+  // let table = document.createElement("table")
+
+  // let headerRow = document.createElement("tr")
+  // headers.forEach(headerText => {
+  //   let header = document.createElement("th")
+  //   let texNode = document.createTextNode(headerText)
+  //   header.appendChild(texNode)
+  //   headerRow.appendChild(header)
+  // })
+  // table.appendChild(headerRow)
 
 // $(document).on("click","#pAdd", function(e){
 
@@ -206,3 +204,72 @@ $(document).on("click","#cAdd", function(e){
 // // items.innerText = order.productName;
 // // list.append(items);
 
+// function addNumberT() {
+//   var x = document.getElementById("pPrice").value;
+//   var y =   document.getElementById("qty '+counter'").value;
+//   var z = document.getElementById("amount").value;
+//   var disc = document.getElementById("disc").value
+//   var discTotal = y * x * disc / 100 ;
+//   document.getElementById("total + '+counter+' ").value = (y * x ) ;
+// }
+// addMore.addEventListener("click", function(e){
+//   counter+=1
+//     e.preventDefault()
+//     let newDiv =  document.createElement('div');
+//     newDiv.setAttribute('class', 'groupT');
+//     tForm.appendChild(newDiv)
+
+//     // let pPrce =  document.createElement('input');
+//     // pPrce.setAttribute('id', 'pPrice' + counter++);
+//     // pPrce.setAttribute('oninput',"addNumberT()");
+//     // newDiv.appendChild(pPrce)
+
+//     // let qty =  document.createElement('input');
+//     // qty.setAttribute('id', 'qty' + counter++);
+//     // qty.setAttribute('type', 'number');
+//     // qty.setAttribute('oninput', "addNumberT()" );
+//     // newDiv.appendChild(qty)
+
+//     // let total =  document.createElement('input');
+//     // total.setAttribute('id', 'total' + counter++);
+    
+//     // newDiv.appendChild(total)
+//     // newDiv.innerHTML= '<input id="pName" name="pName" type="text" placeholder="Products name">\
+//     // <input id="productId" name="productId" type="text" placeholder="Product id">\
+//     // <input id="pPrice'+counter+' "  oninput="addNumber()" name="pPrice" type="text" placeholder="Selling price">\
+//     // <input id="qty'+counter+' "  oninput="addNumber()" type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="qty " size="4" pattern="" inputmode="" placeholder="qty">\
+//     // <input id="total'+counter+'" name="total" type="text" placeholder="Total: ">\
+//     // '
+
+//     newDiv.innerHTML= `<input id="pName" name="pName" type="text" placeholder="Products name">
+//     <input id="productId" name="productId" type="text" placeholder="Product id">
+//     <input id="pPrice'+counter+' "  oninput="addNumber()" name="pPrice" type="text" placeholder="Selling price">
+//     <input id="qty'+counter+' "  oninput="addNumber()" type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="qty " size="4" pattern="" inputmode="" placeholder="qty">
+//     <input id="disc +counter+' " oninput="addNumber()" name="disc" type="text" placeholder="Discount:">
+//     <input id="amount'+counter+' " oninput="addNumber()" name="amount" type="text" placeholder="Amount:">
+//     <input id="stock" name="stock" type="text" placeholder="stock:">
+//     <input id="total'+counter+'" name="total" type="text" placeholder="Total: ">
+//     <button type="button" id="tAdd" class="add"><i class="fas fa-trash"></i></button>`
+
+    
+    
+
+//     newDiv.addEventListener("click", function(e){
+//         const item = e.target;
+//         if(item.classList[0]==="add"){
+//             const dlt = item.parentElement;
+//             dlt.classList="remove";
+//             dlt.addEventListener("transitioned",function(){
+//               dlt.remove();
+//             });
+//           }
+         
+//         // newDiv.classList='remove'
+//     })
+
+// })
+
+
+
+
+// let headers = ["Product Name" , "Price" ,"Qty" ,"Total"] 
